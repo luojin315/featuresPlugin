4f9d9d688240baf8af49c3ef8818908b0999c691
luojin <773559646@qq.com>
2021-12-13 23:58:56 +0800

    feat:增加eslint+prettier配置约束统一代码风格

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -22,4 +26,14 @@ function App() {
   );
 }
 
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,11 +1,14 @@
+// 博客说明：https://note.xiexuefeng.cc/post/about-web-vitals/
+// git:https://github.com/GoogleChrome/web-vitals
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      getLCP(onPerfEntry); // 最大内容渲染
+      getFCP(onPerfEntry); // 第一次内容绘制
+      getCLS(onPerfEntry); // 首屏页面渲染过程中所有元素结点相对原始位置所发生的位置偏移累积量
+      getFID(onPerfEntry); // 用户交互触发的事件到主线程接收事件之间的延迟
+      getTTFB(onPerfEntry); // 浏览器从开始加载网页内容到接收到第一个字节的网页内容之间的耗时
     });
   }
 };
